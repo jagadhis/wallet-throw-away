bb80a4a3549cd7232fa191fae4d0c9f0f98f70ad
jagadhis <jwaran78@gmail.com>
2024-10-14 12:39:35 +0530

    fix: pwa app changes

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@ import withPWA from 'next-pwa';
 
 export default withPWA({
     dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
     register: true,
     scope: '/',
     sw: 'service-worker.js',
